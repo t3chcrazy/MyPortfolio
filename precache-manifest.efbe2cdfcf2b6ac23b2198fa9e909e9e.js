5ca33b8a2bb43ccbf243cee1acfffe701addc247
Abhishek Prashant <suman4283@gmail.com>
2019-12-07 10:48:12 +0530

    Updates

---
+++ precache-manifest.efbe2cdfcf2b6ac23b2198fa9e909e9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3084f1d74b07dff150a1451850e39f8d",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "35c8ae4b1edc436d9b92",
+    "url": "/MyPortfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c2ec633d826ab41ffe0d",
+    "url": "/MyPortfolio/static/js/2.7edf8f5d.chunk.js"
+  },
+  {
+    "revision": "35c8ae4b1edc436d9b92",
+    "url": "/MyPortfolio/static/js/main.9405fc8c.chunk.js"
+  },
+  {
+    "revision": "45c2ad1536934e1aca4c",
+    "url": "/MyPortfolio/static/js/runtime-main.bf56be72.js"
+  },
+  {
+    "revision": "f2f706d3db08a48344ee37185e510044",
+    "url": "/MyPortfolio/static/media/abhishek-prashant.f2f706d3.jpg"
+  }
+]);
